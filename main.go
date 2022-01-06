d33951209f0031670d40a5f46f8820ba9ff20157
thinkcats <thinkcat@icloud.com>
2022-01-07 01:10:32 +0800

    move comment

--- main.go
+++ main.go
@@ -8,36 +8,6 @@ import (
 )
 
 func main() {
-	//	app := &cli.App{
-	//		Name:  "neodb",
-	//		Usage: "a simple demo database",
-	//		Flags: []cli.Flag{
-	//			&cli.StringFlag{
-	//				Name:    "host",
-	//				Aliases: []string{"H"},
-	//				Usage:   "db server host address",
-	//			},
-	//			&cli.StringFlag{
-	//				Name:    "port",
-	//				Aliases: []string{"P"},
-	//				Usage:   "db server port",
-	//			},
-	//			&cli.StringFlag{
-	//				Name:    "user",
-	//				Aliases: []string{"U"},
-	//				Usage:   "db server user name",
-	//			},
-	//			&cli.StringFlag{
-	//				Name:    "password",
-	//				Aliases: []string{"pwd"},
-	//				Usage:   "db server password",
-	//			},
-	//		},
-	//		Action: func(_ *cli.Context) error {
-	//			fmt.Println("Hello NeoDB")
-	//			return nil
-	//		},
-	//	}
 
 	app := cli.CliConfig()
 	err := app.Run(os.Args)
